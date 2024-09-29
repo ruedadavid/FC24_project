9a5a3144e81b70553b1c2a953a4a7b156255f52c
David Alonso Rueda Rodríguez <daruedar@correo.udistrital.edu.co>
2024-09-28 17:34:10 -0700

    Feat. Add function to read routes from config file

--- src/fc_scrapper.py
+++ src/fc_scrapper.py
@@ -1,8 +1,9 @@
 """ Scrapper for FC25
 """
-from library import read_config_file
-from pathlib import Path
+from pathlib import PosixPath
 import typer
+from library import read_config_file
+
 #from requests import request
 
 
@@ -12,9 +13,10 @@ def main() -> None:
     """
     print('Test 1.')
     src_dir = '/home/davo/Projects/FC24_project/src'
+    src_route = PosixPath(src_dir)
     config_file = 'app.messages.conf'
-    config_file = Path.joinpath(Path(src_dir), config_file)
-    rut = read_config_file(filename=config_file, features='ROUTES')
+    config_file_route = PosixPath.joinpath(src_route, config_file)
+    rut = read_config_file(filename=config_file_route, features='ROUTES')
     print(rut)
 
 
