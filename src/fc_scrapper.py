27d44c1c950b9927dbd27b62efedbcd88bb59aab
David Alonso Rueda Rodríguez <daruedar@correo.udistrital.edu.co>
2024-09-29 19:02:50 -0700

    Fix. Simolify the loop that controls the visit of each page

--- src/fc_scrapper.py
+++ src/fc_scrapper.py
@@ -9,38 +9,96 @@ from bs4 import BeautifulSoup
 from library import read_config_file
 
 
+def get_limit(
+    soup: BeautifulSoup,
+    name:str,
+    class_:str
+) -> int:
+    """fname _summary_
+
+    Parameters
+    ----------
+    arg : datatype
+        _description_
+
+    Returns
+    -------
+    type
+        _description_
+    """
+    finds = soup.find_all(
+        name = name,
+        class_ = class_
+    )
+    return int(finds[-1].text)
+
 def main() -> None:
     """main _summary_
     """
     src_dir = '/home/davo/Projects/FC24_project/src'
     src_route = PosixPath(src_dir)
+    url = 'web_url'
     config_file = 'app.messages.conf'
     config_file_route = PosixPath.joinpath(src_route, config_file)
     #
     rut = read_config_file(filename=config_file_route, features='DATA')
-    url = 'web_url'
     routes = read_config_file(filename=config_file_route, features='ROUTES')
     scrappers = read_config_file(filename=config_file_route, features='SCRAPPER')
-    for page in range(int(rut['max_pages'])):
-        count = page + 1
-        complete_route = f'{rut[url]}{count}'
-        page = requests.get(complete_route, timeout=30)
-        print(scrappers['scrapper_max'])
+    out_dir = PosixPath(routes['output_dir'])
+    #
+    ea_page = 0
+    ea_pages = 200
+    while True:
+        ea_page = ea_page + 1
+        complete_route = f'{rut[url]}{ea_page}'
+        req = requests.get(complete_route, timeout=30)
+        soup = BeautifulSoup(
+                markup = req.content,
+                features = 'html.parser'
+        )
+        print(f'{ea_page}')
+        if req.status_code == 200:
+            if ea_page == 1:
+                limit = get_limit(
+                    soup,
+                    scrappers['scrapper_max_type'],
+                    scrappers['scrapper_max']
+                )
+                print(f'Páginas { limit }-{ ea_page }')
+            if ea_page == limit:
+                print(f'P+aginas { ea_page }')
+                break
+        if ea_page == ea_pages:
+            break
+    for ea_page in range(int(rut['max_pages'])):
+        complete_route = f'{rut[url]}{ea_page + 1}'
         #
-        print(routes)
-        #
-        if page.status_code == 200:
-            soup = BeautifulSoup(page.content, 'html.parser')
-            limits = soup.find_all('span', class_=scrappers['scrapper_max'])
-            limit = int(limits[-1].text)
-            print(f"\n{limit}\n")
+        if req.status_code == 200:
+            output_file = PosixPath.joinpath(
+                out_dir,
+                f'file{ea_page + 1}.json'
+            )
+            soup = BeautifulSoup(
+                markup = req.content,
+                features = 'html.parser'
+            )
+            if ea_page == 0:
+                limits = soup.find_all(
+                    name = scrappers['scrapper_max_type'],
+                    class_ = scrappers['scrapper_max'],
+                )
+                limit = int(limits[-1].text)
+                print(f"\n{limit}\n")
             link_list = []
             #
-            for link in soup.find_all("a", class_=scrappers['player_link']):
-                link_list.append(link)
-            list02 = [link['href']for link in link_list]
-            out_dir = PosixPath(routes['output_dir'])
-            output_file = PosixPath.joinpath(out_dir, f'file{count}.json')
+            player_cards = soup.find_all(
+                name = scrappers['player_class_type'],
+                class_ = scrappers['player_link']
+            )
+            link_list = [link for link in player_cards]
+            list02 = [link['href'] for link in link_list]
+            #
+            #
             with open(output_file.resolve(), 'w', encoding='utf-8') as f:
                 json.dump(list02, f, indent=2)
             #
